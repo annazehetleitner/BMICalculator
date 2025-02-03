b5da46896b3bdb6e00b5b9c0f8f015c816fc66cb
Anna Zehetleitner <52400831@edu.campus02.at>
2025-02-03 16:57:19 +0100

    Add tests for calculateBMI

--- src/test/java/com/bmicalculator/BMICalculatorTest.java
+++ src/test/java/com/bmicalculator/BMICalculatorTest.java
@@ -16,7 +16,13 @@ public class BMICalculatorTest {
     }
     @Test
     public void testCalculateBMI(){
-        BMICalculator bmiCalc = new BMICalculator("Test", "User", 170, 70, 'm');
+        BMICalculator bmiCalc = new BMICalculator("Susi", "Sorglos", 170, 70, 'w');
         assertEquals(24.22, bmiCalc.calculateBMI(), 0.01);
+    }
+    @Test
+    public void testCalculateBMIWithHighWeight(){
+        BMICalculator bmiCalc = new BMICalculator("Max", "Mustermann", 180, 120, 'm');
+        assertEquals(37.04, bmiCalc.calculateBMI(), 0.01);
+
     }
 }
