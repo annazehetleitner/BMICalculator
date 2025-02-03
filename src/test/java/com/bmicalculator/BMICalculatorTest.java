8501aa44b0f900e0082fb70f43ab5f3c685800d6
Anna Zehetleitner <52400831@edu.campus02.at>
2025-02-03 15:50:31 +0100

    Test Konstruktor

--- src/test/java/com/bmicalculator/BMICalculatorTest.java
+++ src/test/java/com/bmicalculator/BMICalculatorTest.java
@@ -1,4 +1,20 @@
 package com.bmicalculator;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class BMICalculatorTest {
+
+    @Test
+    public void testContructor(){
+        BMICalculator bmiCalc = new BMICalculator("Max", "Mustermann", 180, 75, 'm');
+        assertEquals("Max", bmiCalc.getFirstname());
+        assertEquals("Mustermann", bmiCalc.getLastname());
+        assertEquals(180, bmiCalc.getBodyHeight());
+        assertEquals(75, bmiCalc.getBodyWeight());
+        assertEquals('m', bmiCalc.getGender());
+    }
+
+
+
 }
